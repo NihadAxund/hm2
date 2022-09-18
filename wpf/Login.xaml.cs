6a592ab5d226564eb1c52e61b21a00fbb83d4dab
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-09-18 19:33:32 +0400

    Add project files.

---
+++ wpf/Login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpf
+{
+    public partial class Login : Window
+    {
+        private Sqlclass sql = new();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) =>Close();
+        
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Sign n = new();
+            MainWindow m = new(n);
+            m.ShowDialog();
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (sql.Check(Name_txt.Text, Password_txt.Text))
+            {
+                MessageBox.Show("Hello");
+            }
+            else MessageBox.Show("No User");
+        }
+    }
+}
---
+++ wpf/Sqlclass.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace wpf
+{
+    public class Sqlclass
+    {
+       private string connectionString = "Data Source=NIHAD; Initial Catalog=Userpassword; Integrated Security=SSPI";
+        SqlConnection sqlConnection = null;
+       public Sqlclass()
+        {
+             sqlConnection = new SqlConnection(connectionString);
+        }
+        public bool Check(string UserName,string password)
+        {
+            using (SqlConnection conn = new(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(@"SELECT * FROM Authors WHERE Id = @id OR FirstName = @UserName", conn);
+                cmd.Parameters.Add("@name", System.Data.SqlDbType.NVarChar).Value = UserName;
+                SqlDataReader reader = cmd.ExecuteReader();
+                return reader.FieldCount != 0;
+            }
+        }
+        public bool AddUser(string UserName, string password)
+        {
+            try
+            {
+                sqlConnection.Open();
+                string insertString = $@"INSERT INTO User (User, Password) VALUES('{UserName}', '{password}')";
+                SqlCommand cmd = new(insertString, sqlConnection);
+                cmd.ExecuteNonQuery();
+                sqlConnection.Close();
+                return true;
+            }
+            catch { return false; }
+            finally
+            {
+                if (sqlConnection != null) sqlConnection.Close();
+            }
+        }
+
+    }
+}
