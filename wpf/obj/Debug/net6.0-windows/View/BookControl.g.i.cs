9182c40ef7f36736d98667619b47cdc5c8656ae5
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-10-18 15:45:35 +0400

    son

--- wpf/obj/Debug/net6.0-windows/View/BookControl.g.i.cs
+++ wpf/obj/Debug/net6.0-windows/View/BookControl.g.i.cs
@@ -86,7 +86,7 @@ namespace wpf.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/wpf;V1.0.0.0;component/view/bookcontrol.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/wpf;component/view/bookcontrol.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\BookControl.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
