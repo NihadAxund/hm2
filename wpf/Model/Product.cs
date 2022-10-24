82fcfb24acf62122fdb732fd26fdbf695e39c43e
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-10-24 18:36:49 +0400

    Library

---
+++ wpf/Model/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.DB
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public double Coust { get; set; }
+    
+        public Product(string name, string country, double coust)
+        {
+            Name = name;
+            Country = country;
+            Coust = coust;
+        }
+        public Product()
+        {
+
+        }
+
+
+    }
+}
